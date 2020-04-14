f7b1c0074a9827560a9b456c6eb14e8c684823c5
ZHOU xiaoyu <1016673080@qq.com>
2020-04-15 00:28:26 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.default')
 @section('content')
   @section('title','主页')
-  <h1>主页</h1>
+<div class="jumbotron">
+  <h1>Hello Laravel</h1>
+  <p class="lead">
+    你现在所看到的是周小雨的学习项目！
+ </p>
+ <p>
+ 一切，将从这里开始。
+ </p>
+ <p>
+ <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+  </p>
+</div>
+
 @endsection
