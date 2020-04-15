5d75e4e0384dfb9e0d3dccb25dcff2d8cdbc55f2
ZHOU xiaoyu <1016673080@qq.com>
2020-04-15 15:48:17 +0800

    中文的错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,7 @@
         <h5>注册</h5>
       </div>
       <div class="card-body">
+        @include('shared._error')
         <form method="POST" action="{{ route('users.store') }}">
           @csrf
           <div class="form-group">
